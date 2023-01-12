de5ae893fe88484c560c6e9c3bb5966d7afaeebe
Sarthak Yelne <sarthakyelne2003@gmail.com>
2023-01-12 20:28:43 +0530

    stage 1

---
+++ __tests__/todo.js
@@ -0,0 +1,12 @@
+const request = require('supertest')
+
+const db = require('../models/index')
+const app = require('../app')
+
+let server, agent;
+
+describe("Todo Test Suite", () => {
+    beforeAll(async () => {
+        
+    })
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+const app = require('./app')
+app.listen(3000, () => {
+    console.log("Started Express Server at port 3000")
+})
