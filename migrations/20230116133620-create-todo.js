df3ef8546da008142d9f770848c63db6268b771b
Sarthak Yelne <sarthakyelne2003@gmail.com>
2023-01-17 12:33:10 +0530

    Final L8

--- migrations/20230116133620-create-todo.js
+++ migrations/20230116133620-create-todo.js
@@ -1,29 +1,22 @@
-/* eslint-disable no-unused-vars */
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName: {
+      title: {
         type: Sequelize.STRING
       },
-      lastName: {
-        type: Sequelize.STRING
+      dueDate: {
+        type: Sequelize.DATEONLY
       },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-
-      },
-      password: {
-        type: Sequelize.STRING
+      completed: {
+        type: Sequelize.BOOLEAN
       },
       createdAt: {
         allowNull: false,
@@ -36,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Todos');
   }
 };
