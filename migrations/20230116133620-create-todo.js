8e0828a13191ff3a4ab3e38d5618027e861d20d4
Sarthak Yelne <sarthakyelne2003@gmail.com>
2023-01-18 09:34:04 +0530

    Final L9 Deploy...

--- migrations/20230116133620-create-todo.js
+++ migrations/20230116133620-create-todo.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-unused-vars */
 "use strict";
-//using strict
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Todos", {
